397993c51d36ab522093f72e0ae3d2eed15dac65
alves2305 <raffa.alves.souza@gmail.com>
2024-08-02 09:56:08 -0300

    projeto-skils

--- script.js
+++ script.js
@@ -12,3 +12,25 @@ function mostrarProjetos(tipo) {
   }
 }
 
+
+function mostrarSkills(tipo) {
+  const backSkills = document.querySelector('.contain-skils-back');
+  const frontSkills = document.querySelector('.contain-skils-front');
+  const ferramentasSkills = document.querySelector('.contain-skils-ferramentas');
+
+  if (tipo === 'back') {
+    backSkills.style.display = 'flex';
+    frontSkills.style.display = 'none';
+    ferramentasSkills.style.display = 'none';
+  } else if (tipo === 'front') {
+    backSkills.style.display = 'none';
+    frontSkills.style.display = 'flex';
+    ferramentasSkills.style.display = 'none';
+  } else if (tipo === 'ferramentas') {
+    backSkills.style.display = 'none';
+    frontSkills.style.display = 'none';
+    ferramentasSkills.style.display = 'flex';
+  }
+}
+
+
